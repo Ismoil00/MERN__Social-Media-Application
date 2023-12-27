50a989a121a2bd89813c9a225b0594de4c6117b7
Ismoil <juraev.abubakr_98@mail.ru>
2023-12-27 10:35:52 +0500

    new codes

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -1,5 +1,5 @@
-import Post from "../models/Post";
-import User from "../models/User";
+import User from "../models/User.js";
+import Post from "../models/Post.js";
 
 /* CREATE */
 export const createPost = async (req, res) => {
@@ -27,3 +27,47 @@ export const createPost = async (req, res) => {
 };
 
 /* READ */
+export const getFeedPosts = async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+export const getUserPosts = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const posts = await Post.find({ userId });
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+/* UPDATE */
+export const likePost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req.body;
+    const post = await Post.findById(id);
+    const isLiked = post.likes.get(userId);
+
+    if (isLiked) {
+      post.likes.delete(userId);
+    } else {
+      post.likes.set(userId, true);
+    }
+
+    const updatedPost = await Post.findByIdAndUpdate(
+      id,
+      { likes: post.likes },
+      { new: true }
+    );
+
+    res.status(200).json(updatedPost);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
